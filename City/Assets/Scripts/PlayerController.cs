5943e867ef06d6b81cddc2c24b1a5314d5e8a0f1
Josh Miller <jmiller@colgate.edu>
2019-11-03 19:13:32 -0500

    init

---
+++ City/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject CamPivot;
+    private Rigidbody rb;
+    public float moveSpeed = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    // For physics code, before physics calculatinos
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void LateUpdate()
+    {
+        Rotate();
+    }
+
+    void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float moveVertical = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        Vector3 movement = (transform.forward) * moveVertical + (transform.right) * moveHorizontal;
+        //movement = movement.normalized;
+        transform.position += movement * moveSpeed;
+    }
+
+    void Rotate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            transform.rotation = CamPivot.transform.rotation;
+        }
+    }
+}
---
+++ City/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public float speed = 1;
+    public GameObject Player;
+    public GameObject CamPivot;
+    public float rotSpeed = 100;
+
+    private Vector3 offset;
+    private float rotDirection = 0;
+    private float vertRotDir = 0;
+    private int rotLimit = 20;
+    private int minDist = 4;
+    private int maxDist = 30;
+
+
+    void Start()
+    {
+        CamPivot.transform.parent = Player.transform;
+        offset = transform.position - CamPivot.transform.position;
+        // to zoom out, multiply; to zoom in, divide
+
+        //Cursor.lockState = CursorLockMode.Locked; //hide cursor; TODO fix for things like menus
+
+    }
+
+    void Update()
+    {
+    }
+
+    void LateUpdate()
+    {
+        HandleCameraZoom();
+        HandleCameraPivot();
+        CamPivot.transform.position = Player.transform.position;
+    }
+
+    void HandleCameraZoom()
+    {
+        if (Input.mouseScrollDelta.y != 0.0f)
+        {
+            if (Input.mouseScrollDelta.y > 0 && Vector3.Distance(transform.position, Player.transform.position) > minDist)
+            {
+                ChangeZoom();
+            }
+            else if (Input.mouseScrollDelta.y < 0 && Vector3.Distance(transform.position, Player.transform.position) < maxDist)
+            {
+                ChangeZoom();
+            }
+        }
+    }
+
+    void HandleCameraPivot()
+    {
+        if (Input.GetMouseButton(1))
+        { // right click
+            rotDirection += Input.GetAxis("Mouse X") * Time.deltaTime * rotSpeed;
+            //rotDirection = BindToRotLimit(rotDirection); //Don't limit x rotation
+            vertRotDir += Input.GetAxis("Mouse Y") * Time.deltaTime * rotSpeed;
+            vertRotDir = BindToRotLimit(vertRotDir);
+
+            CamPivot.transform.rotation = Quaternion.Euler(-1 * vertRotDir, rotDirection, 0);
+        }
+    }
+
+    float BindToRotLimit(float rotDir) {
+        if (rotDir > rotLimit)
+        {
+            rotDir = rotLimit;
+        }
+        if (rotDir < -1 * rotLimit)
+        {
+            rotDir = -1 * rotLimit;
+        }
+        return rotDir;
+    }
+
+    void ChangeZoom()
+    {
+        offset = transform.position - CamPivot.transform.position;
+        offset *= (10 - Input.mouseScrollDelta.y) / 10;
+        transform.position = CamPivot.transform.position + offset;
+
+    }
+
+}
