7d12715df02c7fa540e858480efa7dd4c0a6ea48
Josh Miller <jmiller@colgate.edu>
2019-11-09 16:50:05 -0500

    Init rhythm system

---
+++ City/Assets/BeatController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatController : MonoBehaviour
+{
+    public float speed;
+    public BeatSpawner ParentSpawner;
+    public int PerfectNote = 200;
+    public int NoteDeviation = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + Time.deltaTime * speed, transform.position.y, transform.position.z);
+        if (transform.position.x > PerfectNote + NoteDeviation)
+        {
+            DeleteBeat();
+        }
+    }
+
+    public void DeleteBeat()
+    {
+        ParentSpawner.Beats.Remove(gameObject);
+        //Debug.Log("Beat faded!");
+        Destroy(gameObject);
+    }
+}
---
+++ City/Assets/BeatSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatSpawner : MonoBehaviour
+{
+    public GameObject BeatPrefab;
+    public float BeatInterval = 1;
+    public List<GameObject> Beats;
+    private float ToNextBeat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ToNextBeat = BeatInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ToNextBeat -= Time.deltaTime;
+        if (ToNextBeat < 0)
+        {
+            SpawnBeat();
+            ToNextBeat = BeatInterval;
+        }
+    }
+
+    void SpawnBeat()
+    {
+        GameObject NewBeat = Instantiate(BeatPrefab, transform);
+        NewBeat.transform.SetParent(gameObject.transform);
+        NewBeat.GetComponent<BeatController>().ParentSpawner = this;
+        Beats.Add(NewBeat);
+    }
+}
